ea36e8750a7436ffb8a176847af955d0a296c4c3
Mounir Dellagi <mounir.dellagi@gmail.com>
2013-02-28 17:07:57 +0100

    Moving more files

--- validatereceipt.m
+++ validatereceipt.m
@@ -47,21 +47,6 @@
 #warning ************************************
 #endif
 
-
-#ifndef YES_I_HAVE_READ_THE_WARNING_AND_I_ACCEPT_THE_RISK
-
-#warning --- DON'T USE THIS CODE AS IS! IF EVERYONE USES THE SAME CODE
-#warning --- IT IS PRETTY EASY TO BUILD AN AUTOMATIC CRACKING TOOL
-#warning --- FOR APPS USING THIS CODE!
-#warning --- BY USING THIS CODE YOU ACCEPT TAKING THE RESPONSIBILITY FOR
-#warning --- ANY DAMAGE!
-#warning ---
-#warning --- YOU HAVE BEEN WARNED!
-
-// if you want to take that risk, add "-DYES_I_HAVE_READ_THE_WARNING_AND_I_ACCEPT_THE_RISK" in the build settings at "Other C Flags"
-
-#endif // YES_I_HAVE_READ_THE_WARNING_AND_I_ACCEPT_THE_RISK
-
 #define VRCFRelease(object) if(object) CFRelease(object)
 
 NSString *kReceiptBundleIdentifier				= @"BundleIdentifier";
