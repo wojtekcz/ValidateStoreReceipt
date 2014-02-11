9c03ebb573f2a5281825294123ed3e4e443cdf9f
Wojciech Czarnowski <wcz@jatar.com.pl>
2014-02-11 11:06:39 +0100

    Converted project to ARC.

--- validatereceipt.m
+++ validatereceipt.m
@@ -7,15 +7,15 @@
 
 /*
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
+ 
  Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-
+ 
  Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in
  the documentation and/or other materials provided with the distribution.
-
+ 
  Neither the name of the copyright holders nor the names of its contributors may be used to endorse or promote products derived
  from this software without specific prior written permission.
-
+ 
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
  BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
  SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
@@ -66,23 +66,23 @@ NSString *kReceiptInAppOriginalPurchaseDate		= @"OriginalPurchaseDate";
 NSData * appleRootCert(void)
 {
 	OSStatus status;
-
+    
 	SecKeychainRef keychain = nil;
 	status = SecKeychainOpen("/System/Library/Keychains/SystemRootCertificates.keychain", &keychain);
 	if(status){
 		VRCFRelease(keychain);
 		return nil;
 	}
-
+    
 	CFArrayRef searchList = CFArrayCreate(kCFAllocatorDefault, (const void**)&keychain, 1, &kCFTypeArrayCallBacks);
-
+    
 	// For some reason we get a malloc reference underflow warning message when garbage collection
 	// is on. Perhaps a bug in SecKeychainOpen where the keychain reference isn't actually retained
 	// in GC?
 #ifndef __OBJC_GC__
 	VRCFRelease(keychain);
 #endif
-
+    
 	SecKeychainSearchRef searchRef = nil;
     // Warnings originate from Apple deprecating dynamic linking of Open SSL. You should statically link Open SSL to get rid of warnings.
     // Or refactor this code to use common crypto. Or just ignore it and hope Open SSL will not change that much in the future
@@ -92,24 +92,24 @@ NSData * appleRootCert(void)
 		VRCFRelease(searchList);
 		return nil;
 	}
-
+    
 	SecKeychainItemRef itemRef = nil;
 	NSData * resultData = nil;
-
+    
 	while(SecKeychainSearchCopyNext(searchRef, &itemRef) == noErr && resultData == nil) {
 		// Grab the name of the certificate
 		SecKeychainAttributeList list;
 		SecKeychainAttribute attributes[1];
-
+        
 		attributes[0].tag = kSecLabelItemAttr;
-
+        
 		list.count = 1;
 		list.attr = attributes;
-
+        
 		SecKeychainItemCopyContent(itemRef, nil, &list, nil, nil);
 		NSData *nameData = [NSData dataWithBytesNoCopy:attributes[0].data length:attributes[0].length freeWhenDone:NO];
 		NSString *name = [[NSString alloc] initWithData:nameData encoding:NSUTF8StringEncoding];
-
+        
 		if([name isEqualToString:@"Apple Root CA"]) {
 			CSSM_DATA certData;
 			SecCertificateGetData((SecCertificateRef)itemRef, &certData);
@@ -117,16 +117,15 @@ NSData * appleRootCert(void)
 		}
 		
 		SecKeychainItemFreeContent(&list, NULL);
-
+        
 		if (itemRef)
 			VRCFRelease(itemRef);
-
-		[name release];
+        
 	}
-
+    
 	VRCFRelease(searchList);
 	VRCFRelease(searchRef);
-
+    
 	return resultData;
 }
 
@@ -228,16 +227,15 @@ NSArray * parseInAppPurchasesData(NSData * inappData)
 							
 							NSNumber *num = [[NSNumber alloc] initWithUnsignedInteger:quantity];
 							[item setObject:num forKey:kReceiptInAppQuantity];
-							[num release];
 						}
 					}
 					
 					// Strings
 					if (attr_type == INAPP_PRODID ||
-							 attr_type == INAPP_TRANSID ||
-							 attr_type == INAPP_ORIGTRANSID ||
-							 attr_type == INAPP_PURCHDATE ||
-							 attr_type == INAPP_ORIGPURCHDATE) {
+                        attr_type == INAPP_TRANSID ||
+                        attr_type == INAPP_ORIGTRANSID ||
+                        attr_type == INAPP_PURCHDATE ||
+                        attr_type == INAPP_ORIGPURCHDATE) {
 						
 						int str_type = 0;
 						long str_length = 0;
@@ -256,12 +254,11 @@ NSArray * parseInAppPurchasesData(NSData * inappData)
 									break;
 							}
 							
-							if (key) {                        
+							if (key) {
 								NSString *string = [[NSString alloc] initWithBytes:str_p
 																			length:(NSUInteger)str_length
 																		  encoding:NSUTF8StringEncoding];
 								[item setObject:string forKey:key];
-								[string release];
 							}
 						}
 						if (str_type == V_ASN1_IA5STRING) {
@@ -274,12 +271,11 @@ NSArray * parseInAppPurchasesData(NSData * inappData)
 									break;
 							}
 							
-							if (key) {                        
+							if (key) {
 								NSString *string = [[NSString alloc] initWithBytes:str_p
 																			length:(NSUInteger)str_length
 																		  encoding:NSASCIIStringEncoding];
 								[item setObject:string forKey:key];
-								[string release];
 							}
 						}
 					}
@@ -302,7 +298,6 @@ NSArray * parseInAppPurchasesData(NSData * inappData)
 		}
 		
 		[resultArray addObject:item];
-		[item release];
 	}
 	
 	return resultArray;
@@ -312,7 +307,7 @@ NSArray * parseInAppPurchasesData(NSData * inappData)
 NSDictionary * dictionaryWithAppStoreReceipt(NSString * path)
 {
 	NSData * rootCertData = appleRootCert();
-
+    
 #define ATTR_START 1
 #define BUNDLE_ID 2
 #define VERSION 3
@@ -320,38 +315,38 @@ NSDictionary * dictionaryWithAppStoreReceipt(NSString * path)
 #define HASH 5
 #define ATTR_END 6
 #define INAPP_PURCHASE 17
-
+    
 	ERR_load_PKCS7_strings();
 	ERR_load_X509_strings();
 	OpenSSL_add_all_digests();
-
+    
 	// Expected input is a PKCS7 container with signed data containing
 	// an ASN.1 SET of SEQUENCE structures. Each SEQUENCE contains
 	// two INTEGERS and an OCTET STRING.
-
+    
 	const char * receiptPath = [[path stringByStandardizingPath] fileSystemRepresentation];
 	FILE *fp = fopen(receiptPath, "rb");
 	if (fp == NULL)
 		return nil;
-
+    
 	PKCS7 *p7 = d2i_PKCS7_fp(fp, NULL);
 	fclose(fp);
-
+    
 	// Check if the receipt file was invalid (otherwise we go crashing and burning)
 	if (p7 == NULL) {
 		return nil;
 	}
-
+    
 	if (!PKCS7_type_is_signed(p7)) {
 		PKCS7_free(p7);
 		return nil;
 	}
-
+    
 	if (!PKCS7_type_is_data(p7->d.sign->contents)) {
 		PKCS7_free(p7);
 		return nil;
 	}
-
+    
 	int verifyReturnValue = 0;
 	X509_STORE *store = X509_STORE_new();
 	if (store)
@@ -362,13 +357,13 @@ NSDictionary * dictionaryWithAppStoreReceipt(NSString * path)
 		{
 			BIO *payload = BIO_new(BIO_s_mem());
 			X509_STORE_add_cert(store, appleCA);
-
+            
 			if (payload)
 			{
 				verifyReturnValue = PKCS7_verify(p7,NULL,store,NULL,payload,0);
 				BIO_free(payload);
 			}
-
+            
 			// this code will come handy when the first real receipts arrive
 #if 0
 			unsigned long err = ERR_get_error();
@@ -382,52 +377,52 @@ NSDictionary * dictionaryWithAppStoreReceipt(NSString * path)
 				}
 			}
 #endif
-
+            
 			X509_free(appleCA);
 		}
 		X509_STORE_free(store);
 	}
 	EVP_cleanup();
-
+    
 	if (verifyReturnValue != 1)
 	{
 		PKCS7_free(p7);
 		return nil;
 	}
-
+    
 	ASN1_OCTET_STRING *octets = p7->d.sign->contents->d.data;
 	const uint8_t *p = octets->data;
 	const uint8_t *end = p + octets->length;
-
+    
 	int type = 0;
 	int xclass = 0;
 	long length = 0;
-
+    
 	ASN1_get_object(&p, &length, &type, &xclass, end - p);
 	if (type != V_ASN1_SET) {
 		PKCS7_free(p7);
 		return nil;
 	}
-
+    
 	NSMutableDictionary *info = [NSMutableDictionary dictionary];
-
+    
 	while (p < end) {
 		ASN1_get_object(&p, &length, &type, &xclass, end - p);
 		if (type != V_ASN1_SEQUENCE)
 			break;
-
+        
 		const uint8_t *seq_end = p + length;
-
+        
 		int attr_type = 0;
 		int attr_version = 0;
-
+        
 		// Attribute type
 		ASN1_get_object(&p, &length, &type, &xclass, seq_end - p);
 		if (type == V_ASN1_INTEGER && length == 1) {
 			attr_type = p[0];
 		}
 		p += length;
-
+        
 		// Attribute version
 		ASN1_get_object(&p, &length, &type, &xclass, seq_end - p);
 		if (type == V_ASN1_INTEGER && length == 1) {
@@ -435,11 +430,11 @@ NSDictionary * dictionaryWithAppStoreReceipt(NSString * path)
 			attr_version = attr_version;
 		}
 		p += length;
-
+        
 		// Only parse attributes we're interested in
 		if ((attr_type > ATTR_START && attr_type < ATTR_END) || attr_type == INAPP_PURCHASE) {
 			NSString *key = nil;
-
+            
 			ASN1_get_object(&p, &length, &type, &xclass, seq_end - p);
 			if (type == V_ASN1_OCTET_STRING) {
                 NSData *data = [NSData dataWithBytes:p length:(NSUInteger)length];
@@ -462,7 +457,7 @@ NSDictionary * dictionaryWithAppStoreReceipt(NSString * path)
                         [info setObject:data forKey:key];
                     }
 				}
-
+                
 				// Strings
 				if (attr_type == BUNDLE_ID || attr_type == VERSION) {
 					int str_type = 0;
@@ -479,12 +474,11 @@ NSDictionary * dictionaryWithAppStoreReceipt(NSString * path)
 								break;
 						}
                         
-						if (key) {                        
+						if (key) {
                             NSString *string = [[NSString alloc] initWithBytes:str_p
 																		length:(NSUInteger)str_length
                                                                       encoding:NSUTF8StringEncoding];
                             [info setObject:string forKey:key];
-                            [string release];
 						}
 					}
 				}
@@ -498,16 +492,16 @@ NSDictionary * dictionaryWithAppStoreReceipt(NSString * path)
 			}
 			p += length;
 		}
-
+        
 		// Skip any remaining fields in this SEQUENCE
 		while (p < seq_end) {
 			ASN1_get_object(&p, &length, &type, &xclass, seq_end - p);
 			p += length;
 		}
 	}
-
+    
 	PKCS7_free(p7);
-
+    
 	return info;
 }
 
@@ -522,29 +516,29 @@ CFDataRef copy_mac_address(void)
 	io_iterator_t			 iterator;
 	io_object_t			   service;
 	CFDataRef				 macAddress = nil;
-
+    
 	kernResult = IOMasterPort(MACH_PORT_NULL, &master_port);
 	if (kernResult != KERN_SUCCESS) {
 		printf("IOMasterPort returned %d\n", kernResult);
 		return nil;
 	}
-
+    
 	matchingDict = IOBSDNameMatching(master_port, 0, "en0");
 	if(!matchingDict) {
 		printf("IOBSDNameMatching returned empty dictionary\n");
 		return nil;
 	}
-
+    
 	kernResult = IOServiceGetMatchingServices(master_port, matchingDict, &iterator);
 	if (kernResult != KERN_SUCCESS) {
 		printf("IOServiceGetMatchingServices returned %d\n", kernResult);
 		return nil;
 	}
-
+    
 	while((service = IOIteratorNext(iterator)) != 0)
 	{
 		io_object_t		parentService;
-
+        
 		kernResult = IORegistryEntryGetParentEntry(service, kIOServicePlane, &parentService);
 		if(kernResult == KERN_SUCCESS)
 		{
@@ -555,10 +549,10 @@ CFDataRef copy_mac_address(void)
 		else {
 			printf("IORegistryEntryGetParentEntry returned %d\n", kernResult);
 		}
-
+        
 		IOObjectRelease(service);
 	}
-
+    
 	return macAddress;
 }
 
@@ -596,56 +590,56 @@ BOOL validateReceiptAtPath(NSString * path)
 	// http://www.craftymind.com/2011/01/06/mac-app-store-hacked-how-developers-can-better-protect-themselves/
 	//
 	// so use hard coded values instead (probably even somehow obfuscated)
-
+    
 	// analyser warning when USE_SAMPLE_RECEIPT is defined (wontfix)
 	NSString *bundleVersion = (NSString*)global_bundleVersion;
 	NSString *bundleIdentifier = (NSString*)global_bundleIdentifier;
 #ifndef USE_SAMPLE_RECEIPT
 	// avoid making stupid mistakes --> check again
 	NSCAssert([bundleVersion isEqualToString:[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"]],
-			 @"whoops! check the hard-coded CFBundleShortVersionString!");
+              @"whoops! check the hard-coded CFBundleShortVersionString!");
 	NSCAssert([bundleIdentifier isEqualToString:[[NSBundle mainBundle] bundleIdentifier]],
-			 @"whoops! check the hard-coded bundle identifier!");
+              @"whoops! check the hard-coded bundle identifier!");
 #else
 	bundleVersion = @"1.0.2";
 	bundleIdentifier = @"com.example.SampleApp";
 #endif
 	NSDictionary * receipt = dictionaryWithAppStoreReceipt(path);
-
+    
 	if (!receipt)
 		return NO;
-
+    
 	NSData * guidData = nil;
 #ifndef USE_SAMPLE_RECEIPT
-	guidData = (NSData*)copy_mac_address();
-
-	if ([NSGarbageCollector defaultCollector])
-		[[NSGarbageCollector defaultCollector] enableCollectorForPointer:guidData];
-	else
-		[guidData autorelease];
-
-	if (!guidData)
-		return NO;
+	guidData = (__bridge NSData*)copy_mac_address();
+    
+//	if ([NSGarbageCollector defaultCollector])
+//		[[NSGarbageCollector defaultCollector] enableCollectorForPointer:guidData];
+//	else
+//		[guidData autorelease];
+//    
+//	if (!guidData)
+//		return NO;
 #else
 	// Overwrite with example GUID for use with example receipt
 	unsigned char guid[] = { 0x00, 0x17, 0xf2, 0xc4, 0xbc, 0xc0 };
 	guidData = [NSData dataWithBytes:guid length:sizeof(guid)];
 #endif
-
+    
 	NSMutableData *input = [NSMutableData data];
 	[input appendData:guidData];
 	[input appendData:[receipt objectForKey:kReceiptOpaqueValue]];
 	[input appendData:[receipt objectForKey:kReceiptBundleIdentifierData]];
-
+    
 	NSMutableData *hash = [NSMutableData dataWithLength:SHA_DIGEST_LENGTH];
 	SHA1([input bytes], [input length], [hash mutableBytes]);
-
+    
 	if ([bundleIdentifier isEqualToString:[receipt objectForKey:kReceiptBundleIdentifier]] &&
-		 [bundleVersion isEqualToString:[receipt objectForKey:kReceiptVersion]] &&
-		 [hash isEqualToData:[receipt objectForKey:kReceiptHash]])
+        [bundleVersion isEqualToString:[receipt objectForKey:kReceiptVersion]] &&
+        [hash isEqualToData:[receipt objectForKey:kReceiptHash]])
 	{
 		return YES;
 	}
-
+    
 	return NO;
 }
