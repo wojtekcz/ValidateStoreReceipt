b17494d53cc7dae35a228745ff56005257352c00
Wojciech Czarnowski <wcz@jatar.com.pl>
2014-02-11 15:37:08 +0100

    EVP_cleanup() removes all ciphers and digests from the table and prevents boost asio SSL initialization in Tuxler. Commented it out.

--- validatereceipt.m
+++ validatereceipt.m
@@ -382,7 +382,8 @@ NSDictionary * dictionaryWithAppStoreReceipt(NSString * path)
 		}
 		X509_STORE_free(store);
 	}
-	EVP_cleanup();
+    /* EVP_cleanup() removes all ciphers and digests from the table and prevents boost asio SSL initialization */
+	// EVP_cleanup();
     
 	if (verifyReturnValue != 1)
 	{
